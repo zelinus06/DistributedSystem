662dc140278074d6417c2bf16f5adb3b8a18238c
ThaiDH <thai06102001@gmail.com>
2024-10-16 22:25:22 +0700

    init project

---
+++ src/test/java/com/distributedsystemsubject/DistributedSystemSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributedsystemsubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
